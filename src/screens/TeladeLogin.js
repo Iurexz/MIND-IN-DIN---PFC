ddd0c4fd9226db0a97ed9f5c7e72d41a6e13fde9
Iure Costa <131163902+Iurexz@users.noreply.github.com>
2024-09-24 15:59:21 -0300

    Mudança da Tela de Login.
     reajustar a cada celular. Android/Ios. Testado em um Motorola e Xiaomi, falta Iphone.

--- src/screens/TeladeLogin.js
+++ src/screens/TeladeLogin.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { KeyboardAvoidingView } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
-import { LinearGradient } from 'expo-linear-gradient'; // Importa o LinearGradient
+import { LinearGradient } from 'expo-linear-gradient';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
 export default function LoginScreen({ navigation }) {
   const [email, setEmail] = useState('');
@@ -9,120 +11,124 @@ export default function LoginScreen({ navigation }) {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <LinearGradient
-      colors={['#124C3E', '#3D7E52', '#124C3E']} // Gradiente de fundo
-      style={styles.container}
-    >
-      {/* Imagem de login */}
-      <Image source={require('../../assets/image/Crypto banking and cryptocurrency wallet.png')} style={styles.image} />
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <LinearGradient
+        colors={['#124C3E', '#3D7E52', '#124C3E']}
+        style={styles.gradient}
+      >
+        {/* Imagem de login */}
+        <Image source={require('../../assets/image/Crypto banking and cryptocurrency wallet.png')} style={styles.image} />
 
-      {/* Caixa branca com bordas arredondadas */}
-      <View style={styles.loginBox}>
-        {/* Título */}
-        <Text style={styles.title}>Login</Text>
+        {/* Caixa branca com bordas arredondadas */}
+        <View style={styles.loginBox}>
+          {/* Título */}
+          <Text style={styles.title}>Login</Text>
 
-        {/* Campo de e-mails */}
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Seu e-mail..."
-            value={email}
-            onChangeText={setEmail}
-          />
-          <FontAwesome name="envelope" size={24} color="black" />
-        </View>
+          {/* Campo de e-mail */}
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Seu e-mail..."
+              value={email}
+              onChangeText={setEmail}
+            />
+            <FontAwesome name="envelope" size={wp('6%')} color="black" />
+          </View>
 
-        {/* Campo de senha */}
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Sua senha..."
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry={!showPassword}
-          />
-          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-            <FontAwesome name={showPassword ? "eye" : "eye-slash"} size={24} color="black" />
+          {/* Campo de senha */}
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Sua senha..."
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry={!showPassword}
+            />
+            <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+              <FontAwesome name={showPassword ? "eye" : "eye-slash"} size={wp('6%')} color="black" />
+            </TouchableOpacity>
+          </View>
+
+          {/* Link para cadastro */}
+          <Text style={styles.registerText}>
+            Ainda não é cadastrado?{' '}
+            <Text style={styles.registerLink} onPress={() => navigation.navigate('SignUp')}>
+              Cadastre-se aqui
+            </Text>
+          </Text>
+
+          {/* Botão de login */}
+          <TouchableOpacity style={styles.loginButton}>
+            <Text style={styles.loginButtonText}>Entrar</Text>
+          </TouchableOpacity>
+
+          {/* Botão Google */}
+          <TouchableOpacity style={styles.googleButton}>
+            <Image
+              source={{ uri: 'https://img.icons8.com/color/96/google-logo.png' }}
+              style={styles.googleIcon}
+            />
           </TouchableOpacity>
         </View>
-
-        {/* Link para cadastro */}
-        <Text style={styles.registerText}>
-          Ainda não é cadastrado?{' '}
-          <Text style={styles.registerLink} onPress={() => navigation.navigate('SignUp')}>
-            Cadastre-se aqui
-          </Text>
-        </Text>
-
-        {/* Botão de login */}
-        <TouchableOpacity style={styles.loginButton}>
-          <Text style={styles.loginButtonText}>Entrar</Text>
-        </TouchableOpacity>
-
-        {/* Botão Google */}
-        <TouchableOpacity style={styles.googleButton}>
-          <Image
-            source={{ uri: 'https://img.icons8.com/color/96/google-logo.png' }}
-            style={styles.googleIcon}
-          />
-        </TouchableOpacity>
-      </View>
-    </LinearGradient>
+      </LinearGradient>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'flex-start',
+    padding: wp('0%'),
+  },
+  gradient: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'flex-start',
-    padding: 20,
   },
   image: {
-    width: 300,
-    height: 300,
-    marginBottom: 20,
-    marginTop: 100, // Ajuste a distância da imagem para o topo
+    width: wp('60%'),
+    height: hp('25%'),
+    marginBottom: hp('5%'),
+    marginTop: hp('12%'),
     alignSelf: 'center',
-
   },
   loginBox: {
     backgroundColor: 'white',
-    borderRadius: 30, // Bordas arredondadas
-    padding: 50,
-    width: '110%', // Corrigido para não ultrapassar a largura da tela
-    height: '60%',
+    borderRadius: 30,
+    padding: wp('10%'),
+    width: wp('100%'),
+    height: hp('60%'),
     alignItems: 'center',
-    elevation: 5, // Sombra no Android
-    shadowColor: '#000', // Sombra no iOS
+    elevation: 5,
+    shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    marginTop: '30%', // Coloca a caixa na parte de baixo da tela
-
+    marginTop: hp('5%'),
   },
   title: {
-    fontSize: 24,
+    fontSize: wp('6%'),
     fontWeight: 'bold',
     color: 'black',
-    marginBottom: 20,
+    marginBottom: hp('2%'),
   },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    borderBottomWidth: 1, // Apenas uma linha embaixo
-    borderBottomColor: '#ccc', // Cor da linha
-    marginBottom: 10,
-    paddingVertical: 10, // Ajuste o espaçamento vertical
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    marginBottom: hp('2%'),
+    paddingVertical: hp('1%'),
     width: '90%',
   },
   input: {
     flex: 1,
-    fontSize: 16,
+    fontSize: wp('4%'),
   },
   registerText: {
     color: 'black',
-    marginTop: 10,
+    marginTop: hp('2%'),
   },
   registerLink: {
     color: '#388E3C',
@@ -130,28 +136,28 @@ const styles = StyleSheet.create({
   },
   loginButton: {
     backgroundColor: '#388E3C',
-    padding: 15,
+    padding: wp('4%'),
     borderRadius: 10,
-    marginTop: 10,
+    marginTop: hp('2%'),
     width: '90%',
     alignItems: 'center',
   },
   loginButtonText: {
     color: 'white',
-    fontSize: 18,
+    fontSize: wp('5%'),
     fontWeight: 'bold',
   },
   googleButton: {
-    marginTop: 20,
-    width: 50,
-    height: 50,
+    marginTop: hp('3%'),
+    width: wp('12%'),
+    height: wp('12%'),
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 25,
+    borderRadius: wp('6%'),
     backgroundColor: 'white',
   },
   googleIcon: {
-    width: 30,
-    height: 30,
+    width: wp('8%'),
+    height: wp('8%'),
   },
 });
